a1bbf44e7e5b74e876cfd6e008c5e0881d256dc3
Mark Canlas <github@htmlism.com>
2018-05-11 10:34:17 -0400

    implemented body echo

--- juxtaphase-web/app/controllers/CompileController.scala
+++ juxtaphase-web/app/controllers/CompileController.scala
@@ -7,7 +7,7 @@ import play.api.mvc._
 
 @Singleton
 class CompileController @Inject()(protected val controllerComponents: ControllerComponents) extends BaseController {
-  def compile: Action[AnyContent] = Action { implicit req: Request[AnyContent] =>
-    Ok("hello world")
+  def compile: Action[String] = Action(parse.text) { implicit req: Request[String] =>
+    Ok(req.body)
   }
 }
