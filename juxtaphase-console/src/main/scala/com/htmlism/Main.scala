75fd4d2c1df3d9e4ffca41f7d7c26aad73fad5b3
Mark Canlas <github@htmlism.com>
2018-05-18 00:58:44 -0400

    even more generic

--- juxtaphase-console/src/main/scala/com/htmlism/Main.scala
+++ juxtaphase-console/src/main/scala/com/htmlism/Main.scala
@@ -5,12 +5,15 @@ import cats.implicits._
 
 object Main extends RunUnsafeSync {
   def mainIo(args: Array[String]): IO[Unit] =
+    mainSync[IO](args)
+
+  def mainSync[F[_] : Sync](args: Array[String]): F[Unit] =
     for {
-      env <- IO { new EnvironmentReader[IO] }
+      env <- Sync[F].pure { new EnvironmentReader[F] }
       src <- env.getSourceFile(args)
       opt <- env.detectDisassemblyOptions
-      cpl =  new CompilerRunner[IO]
-      dis =  new DisassemblerRunner[IO]
+      cpl =  new CompilerRunner[F]
+      dis =  new DisassemblerRunner[F]
         _ <- cpl.runCompilerWithSbt(src) >>= dis.findClassFiles >>= dis.disassemble(opt)
     } yield ()
 }
