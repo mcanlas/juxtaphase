9b461f82fa4bb1d3ef4e4aae1ceac09dcb2f7fd3
Mark Canlas <github@htmlism.com>
2018-05-18 00:10:07 -0400

    construct inline

--- juxtaphase-console/src/main/scala/com/htmlism/Main.scala
+++ juxtaphase-console/src/main/scala/com/htmlism/Main.scala
@@ -6,13 +6,13 @@ import cats.implicits._
 object Main extends RunUnsafeSync {
   def mainIo(args: Array[String]): IO[Unit] =
     withEnvReader { env =>
-      withRunnersIo { (cpl, dis) =>
-        for {
-          src <- env.getSourceFile(args)
-          opt <- env.detectDisassemblyOptions
-            _ <- cpl.runCompilerWithSbt(src) >>= dis.findClassFiles >>= dis.disassemble(opt)
-        } yield ()
-      }
+      for {
+        src <- env.getSourceFile(args)
+        opt <- env.detectDisassemblyOptions
+        cpl =  new CompilerRunner[IO]
+        dis =  new DisassemblerRunner[IO]
+          _ <- cpl.runCompilerWithSbt(src) >>= dis.findClassFiles >>= dis.disassemble(opt)
+      } yield ()
     }
 
   private def withEnvReader[A](f: EnvironmentReader[IO] => IO[A]) =
--- juxtaphase-console/src/main/scala/com/htmlism/RunUnsafeSync.scala
+++ juxtaphase-console/src/main/scala/com/htmlism/RunUnsafeSync.scala
@@ -5,9 +5,6 @@ import cats.effect._
 trait RunUnsafeSync {
   def mainIo(args: Array[String]): IO[Unit]
 
-  def withRunnersIo(f: (CompilerRunner[IO], DisassemblerRunner[IO]) => IO[Unit]): IO[Unit] =
-    f(new CompilerRunner, new DisassemblerRunner)
-
   def main(args: Array[String]): Unit =
     mainIo(args).unsafeRunSync()
 }
