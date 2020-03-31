909d642b4494343086487752dcd49833b21751a1
Mark Canlas <github@htmlism.com>
2020-03-31 15:33:36 -0400

    fmt

--- juxtaphase-web/app/controllers/CompileController.scala
+++ juxtaphase-web/app/controllers/CompileController.scala
@@ -5,7 +5,7 @@ import javax.inject._
 import play.api.mvc._
 
 @Singleton
-class CompileController @Inject()(protected val controllerComponents: ControllerComponents) extends BaseController {
+class CompileController @Inject() (protected val controllerComponents: ControllerComponents) extends BaseController {
   def compile: Action[String] = Action(parse.text) { implicit req: Request[String] =>
     Ok(req.body)
   }
