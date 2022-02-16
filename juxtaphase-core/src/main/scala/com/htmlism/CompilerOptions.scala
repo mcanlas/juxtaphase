f9143622d4c49ee08bb3363cf5e03956ee738286
Mark Canlas <github@htmlism.com>
2022-02-15 19:02:01 -0500

    upgrade scala

--- juxtaphase-core/src/main/scala/com/htmlism/CompilerOptions.scala
+++ juxtaphase-core/src/main/scala/com/htmlism/CompilerOptions.scala
@@ -2,7 +2,7 @@ package com.htmlism
 
 object CompilerOptions {
   private val defaultScalaVersion =
-    "2.12.6"
+    "2.13.8"
 
   val empty: CompilerOptions =
     CompilerOptions(defaultScalaVersion, Set.empty)
